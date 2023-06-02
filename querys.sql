58cb3d34c77b8ca1a1c38cb14509de0d32f3f526
Kevin Torres <kevin.torres@devsu.com>
2023-06-02 15:02:07 -0500

    add queries and test file for codigo.js

--- querys.sql
+++ querys.sql
@@ -1,1 +1,15 @@
 -- Escribe tus querys acá
+-- Consulta 1: 
+SELECT Cliente.correo
+FROM Cliente
+JOIN Compra ON Cliente.id_cliente = Compra.id_cliente
+WHERE Compra.id_pasarela = 'transbank'
+GROUP BY Cliente.id_cliente
+HAVING COUNT(*) > 1;
+
+-- Consulta 2: 
+SELECT * 
+FROM Compra c, Informe i
+WHERE c.id_informe = i.id_informe
+    AND c.id_cliente = (SELECT id_cliente FROM Cliente WHERE correo = 'juan_daniel@gmail.com')
+    AND i.estado = 'entregado';
