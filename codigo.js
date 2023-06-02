ca57891bc089aabdd4552e70202d1e74663896f8
Kevin Torres <kevin.torres@devsu.com>
2023-06-02 14:42:07 -0500

    add entity-relationship map and exercise 1

--- codigo.js
+++ codigo.js
@@ -1,56 +1,66 @@
 // Lista de estados posibles para una transferencia.
 const statuses = [
-    {
-        id: 1,
-        name: 'CREADA'
-    },
-    {
-        id: 2,
-        name: 'PAGADA'
-    },
-    {
-        id: 3,
-        name: 'ABORTADA'
-    },
-    {
-        id: 4,
-        name: 'FINALIZADA'
-    }
+  {
+    id: 1,
+    name: "CREADA",
+  },
+  {
+    id: 2,
+    name: "PAGADA",
+  },
+  {
+    id: 3,
+    name: "ABORTADA",
+  },
+  {
+    id: 4,
+    name: "FINALIZADA",
+  },
 ];
 
 // Lista de transferencias inicial
 const transfers = [
-    {
-        id: 1,
-        licensePlate: 'LFTS34',
-        email: 'usuario1@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 2,
-        licensePlate: 'LFTS35',
-        email: 'usuario1@autored.cl',
-        status: 'ABORTADA',
-    },
-    {
-        id: 3,
-        licensePlate: 'BDLS99',
-        email: 'usuario3@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 4,
-        licensePlate: 'LFTS34',
-        email: 'usuario4@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 5,
-        licensePlate: 'BDLS99',
-        email: 'usuario5@autored.cl',
-        status: 'FINALIZADA',
-    }
+  {
+    id: 1,
+    licensePlate: "LFTS34",
+    email: "usuario1@autored.cl",
+    status: "CREADA",
+  },
+  {
+    id: 2,
+    licensePlate: "LFTS35",
+    email: "usuario1@autored.cl",
+    status: "ABORTADA",
+  },
+  {
+    id: 3,
+    licensePlate: "BDLS99",
+    email: "usuario3@autored.cl",
+    status: "CREADA",
+  },
+  {
+    id: 4,
+    licensePlate: "LFTS34",
+    email: "usuario4@autored.cl",
+    status: "CREADA",
+  },
+  {
+    id: 5,
+    licensePlate: "BDLS99",
+    email: "usuario5@autored.cl",
+    status: "FINALIZADA",
+  },
+  {
+    id: 6,
+    licensePlate: "PFCB00",
+    email: "test@example.com",
+    status: "PAGADA",
+  },
 ];
 
+// Escribe tu codigo acá
 
-// Escribe tu codigo acá 
+// Ejercicio 1
+export function getTransfersByEmail(email) {
+  return transfers.filter((transfer) => transfer.email === email);
+}
